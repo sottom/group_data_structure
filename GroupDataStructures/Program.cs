913f439fc63065d0f4e5e41a50b9868ac17afc64
Ammon <ammonj_22@hotmail.com>
2017-09-28 14:31:45 -0600

    Menu

--- GroupDataStructures/Program.cs
+++ GroupDataStructures/Program.cs
@@ -10,11 +10,32 @@ namespace GroupDataStructures
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
-<<<<<<< HEAD
+            int iMainMenuChoice = 0;
+            int iDataStructure = 0;
+
+            while(iMainMenuChoice != 4)
+            {
+                Console.WriteLine("1. Stack");
+                Console.WriteLine("2. Queue");
+                Console.WriteLine("3. Dictionary");
+                Console.WriteLine("4. Exit");
+                Console.Read();
+
+                switch (iMainMenuChoice)
+                {
+                    case 1:
+                        //Stack Stuff
+                        Console.WriteLine("Case 1");
+                        break;
+                    case 2:
+                        Console.WriteLine("Case 2");
+                        break;
+                    default:
+                        Console.WriteLine("Default case");
+                        break;
+
+                }
+            }
         }
-=======
-            Console.WriteLine("What's up, white boys!);        }
->>>>>>> 72710fb17fa1e4bb030c5a920167b05ec4aa19d9
     }
 }
