d19189c8bfc19facfb3b113528bc2470790553cd
Ammon <ammonj_22@hotmail.com>
2017-09-28 14:23:18 -0600

    I hate this right now

--- GroupDataStructures/Program.cs
+++ GroupDataStructures/Program.cs
@@ -11,7 +11,6 @@ namespace GroupDataStructures
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
-            Console.WriteLine("Hi guys!");
         }
     }
 }
